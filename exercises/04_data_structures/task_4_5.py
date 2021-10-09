01158471905cfc12e72a8e70a4353736c7ac5a3e
mamaev-v <vova-mamaev@yandex.ru>
2021-10-09 23:18:16 +0300

    Done 4.5

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,7 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+command1_vlans = command1.split()[-1].split(",")
+command2_vlans = command2.split()[-1].split(",")
+result = set(command1_vlans) & set(command2_vlans)
+print(result)
