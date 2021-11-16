8ec9f8c23f098509135709d9b7800804d31ee3a7
mamaev-v <vova-mamaev@yandex.ru>
2021-11-16 22:52:19 +0300

    Done 6.2b

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,32 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+while True:
+    ip_address = input("Введите IP адрес (прим. 10.0.1.1): ")
+    ip = ip_address.split(".")
+    check = True
+    if len(ip) == 4:
+        for oct in ip:
+            if not (oct.isdigit and 0 <= int(oct) <= 255):
+                check = False
+                break
+    else:
+        check = False
+    if check == True: # or "if check"
+        break
+    print("IP введен не верно")
+
+print("IP  введен верно")
+
+if 0 < int(ip[0]) < 224:
+    print("unicast")
+elif 223 < int(ip[0]) < 240:
+    print("multicast")
+elif ip_address == "255.255.255.255":
+    print("local broadcast")
+elif ip_address == "0.0.0.0":
+    print("unassigned")
+else:
+    print("unused")
+    
